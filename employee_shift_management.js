7251524b7338e15620d3ba56c65877692c84db69
AdityaSharma42 <adityasharma3@usf.edu>
2024-09-22 17:21:08 -0400

    Create listAvailableEmployees function

--- employee_shift_management.js
+++ employee_shift_management.js
@@ -1,11 +1,11 @@
 
 const employees = [
 
-{ name: 'Peter', shifts: [{ day: 'Monday', hours: 3 }, { day: 'Wednesday', hours: 8 }] },
+{ name: 'Peter', shifts: [{ day: 'Monday', hours: 4 }, { day: 'Thursday', hours: 1 }] },
     
-{ name: 'Vaugh', shifts: [{ day: 'Tuesday', hours: 5 }, { day: 'Thursday', hours: 4 }] },
+{ name: 'Vaugh', shifts: [{ day: 'Tuesday', hours: 5 }, { day: 'Thursday', hours: 2 }] },
     
-{ name: 'Steve', shifts: [{ day: 'Monday', hours: 7 }] },
+{ name: 'Steve', shifts: [{ day: 'Wednesday', hours: 7 },{day: 'Thursday', hours: 3}] },
     
 { name: 'Rhodes', shifts: [{ day: 'Friday', hours: 6 }] }
     
@@ -19,6 +19,9 @@ function displayEmployeeShifts(employees){
         });
     }
 displayEmployeeShifts(employees[0]);
+displayEmployeeShifts(employees[1]);
+displayEmployeeShifts(employees[2]);
+displayEmployeeShifts(employees[3]);
 
 function assignShift(employeeName, day, hours) {
     const employee= employees.find (employee=> employee.name == employeeName);
@@ -58,6 +61,28 @@ calculateTotalHours("Vaugh");
 calculateTotalHours("Steve");
 calculateTotalHours("Rhodes");
 calculateTotalHours ("Val");
+
+function listAvailableEmployees (day) {
+        const AvailableEmployees= employees.filter(employee=>{
+            const alreadyassigned= employee.shifts.some(shift=>shift.day==day);
+            return !alreadyassigned;
+
+        });
+        if (AvailableEmployees.length==0){
+            console.log (`No employees are available on ${day}.`);
+            return;
+        }
+        console.log (`Employees available on ${day}:`);
+        AvailableEmployees.forEach(employee=> console.log(employee.name));
+    }
+listAvailableEmployees("Monday");
+listAvailableEmployees("Tuesday");
+listAvailableEmployees("Wednesday");
+listAvailableEmployees("Thursday");
+listAvailableEmployees("Friday");
+
+
+
 
 
 
